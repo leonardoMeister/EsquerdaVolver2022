2c52b8f8b5ca33ffb1a2fefedc488fb5537db0e9
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-02-18 14:48:45 -0300

    Add project files.

---
+++ EsquerdaVolver.ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EsquerdaVolver.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            while (true)
+            {
+                Console.WriteLine("Informe o limite de comandos: Ex(3,7 e 0 para parar)");
+                int numComandos = Convert.ToInt32(Console.ReadLine());
+                if (numComandos == 0) break;
+                Console.WriteLine("Informe o Comando: Ex(DDED)");
+                string comandos = Console.ReadLine().ToLower();; 
+                string direcao = "n";
+
+                for (int i = 0; i < numComandos; i++)
+                {
+                    if (comandos.Length < numComandos)
+                    {
+                        if (i == comandos.Length) break;
+                    }
+
+                    if (comandos[i] == 'd')
+                    {
+                        if (direcao == "s") direcao = "o";
+                        else if (direcao == "n") direcao = "l";
+                        else if (direcao == "l") direcao = "s";
+                        else if (direcao == "o") direcao = "n";
+                    }
+                    else if (comandos[i] == 'e')
+                    {
+                        if (direcao == "s") direcao = "l";
+                        else if (direcao == "n") direcao = "o";
+                        else if (direcao == "l") direcao = "n";
+                        else if (direcao == "o") direcao = "s";
+                    }
+
+                }
+                Console.WriteLine(direcao);
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+
+        }
+    }
+}
